adc8c33635c54d14195a82f4e6c5ba531921a49a
ss <csperichon@gmail.com>
2022-07-19 13:49:10 -0300

    V1.0

--- main.js
+++ main.js
@@ -1,43 +1,94 @@
 import './style.css';
 
-const search_btn = document.querySelector('#search_btn');
-const form = document.querySelector('#form');
-
-search_btn.addEventListener('click', async (e) => {
-  e.preventDefault();
-  const city = await searchLocation();
-  displayData(city);
-  // form.reset();
-});
-
-function kelvinToCelsius(temperature) {
-  return temperature - 273.15;
+// New city constructor
+function NewSearch(city) {
+  const name = city.name;
+  const temp = city.main.temp;
+  const kelvinToCelsius = (temperature) => {
+    return temperature - 273.15;
+  };
+  const celsiusToFahrenheit = (temperature) => {
+    return (temperature * (9 / 5) + 32).toFixed(2);
+  };
+  const kelvinToFahrenheit = (temperature) => {
+    return ((temperature - 273.15) * (9 / 5) + 32).toFixed(2);
+  };
+  return {
+    name,
+    temp,
+    kelvinToCelsius,
+    celsiusToFahrenheit,
+    kelvinToFahrenheit,
+  };
 }
 
-function kelvinToFahrenheit(temperature) {
-  return (temperature - 273.15) * (9 / 5) + 32;
+// Display the search info
+function displayCity(city) {
+  const cityName = document.querySelector('.city_name');
+  cityName.textContent = `The city is: ${city.name}`;
+  const temp = document.querySelector('.city_temp');
+  temp.textContent = `Temperature: ${city.kelvinToCelsius(city.temp)} °C`;
+  changeUnit(city);
 }
 
-function displayData(json) {
-  const cityName = document.createElement('h1');
-  cityName.textContent = `The city is: ${json.name}`;
-  const temp = document.createElement('h2');
-  temp.textContent = `Temperature: ${kelvinToCelsius(json.main.temp).toFixed(
-    2,
-  )} °C`;
-  document.body.append(cityName);
-  document.body.append(temp);
-}
-
+// Fetch the search
 async function searchLocation() {
+  // Search input
   let search = document.querySelector('input[name=search]').value;
   let response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=73b3446ddd2e551f87e99fcb8b8ade42
   `,
     { mode: 'cors' },
   );
-  let json = await response.json();
-  return json;
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(response.message);
+  }
 }
 
-// displayData(searchLocation());
+document.addEventListener('DOMContentLoaded', () => {
+  const city_container = document.querySelector('.city_container');
+  city_container.classList.remove('active');
+});
+
+const search_btn = document.querySelector('#search_btn');
+const form = document.querySelector('#form');
+// Display search data
+search_btn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const city = await searchLocation();
+  const newCity = await NewSearch(city);
+  displayCity(newCity);
+  const city_container = document.querySelector('.city_container');
+
+  // Opacity functionality class
+  city_container.classList.add('active');
+  form.reset();
+});
+
+// Change units in display
+const change_unit_btn = document.querySelector('#change_unit');
+
+function changeUnit(city) {
+  change_unit_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Display Fahrenheit
+    if (change_unit_btn.textContent === 'Temp in Fahrenheit') {
+      const temp = document.querySelector('.city_temp');
+      const actualTemp = city.temp;
+      temp.textContent = `Temperature: ${city.kelvinToFahrenheit(
+        actualTemp,
+      )} °F`;
+      change_unit_btn.textContent = 'Temp in Celsius';
+
+      // Display Celsius
+    } else {
+      const temp = document.querySelector('.city_temp');
+      const actualTemp = city.temp;
+      temp.textContent = `Temperature: ${city.kelvinToCelsius(actualTemp)} °C`;
+      change_unit_btn.textContent = 'Temp in Fahrenheit';
+    }
+  });
+}
